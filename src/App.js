d71cb5cfd78f9d637bf4765ed5fbd954eeabafe5
krasnovsa <65534182+krasnovsa@users.noreply.github.com>
2020-05-23 12:31:54 +0300

    дописать навбар

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
+import Routes from 'routes'
+import {BrowserRouter as Router} from 'react-router-dom'
+import TopBar from 'components/top-bar'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Resource Store </h1>
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router> 
     </div>
   );
 }
+ 
